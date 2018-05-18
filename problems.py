5837aab6098824990772e27765f01d441b96e85b
Damian <dcamenisch98@gmail.com>
2018-05-18 14:53:28 +0200

    stuff

--- problems.py
+++ problems.py
@@ -1,3 +1,4 @@
+import itertools as it
 
 listing = [1, 3, 5, 4, 2, 6, 4, 9]
 new_list = []
@@ -29,4 +30,41 @@ listt = [["asd"], ["sc"], ["dw"], ["a"]]
 for lists in listt:
     listt.remove(lists)
 
-print(listt)
+max_length = 1
+final = []
+final.append([max_length])
+list_to_combine = []
+list_with_numbers = list(range(1, max_length))  # [1, 2, 3, 4, 5]
+length = len(list_with_numbers)
+for nr in range(1, length + 1):  # [1, 2, 3, 4, 5]
+    for time in range(max_length // nr):
+        list_to_combine.append(nr)
+print(list_to_combine)
+list_to_permute = []
+for nr_of_moves in range(1, max_length + 1):
+    combs = list(it.combinations(list_to_combine, nr_of_moves))
+    combs = set(combs)
+    print(nr_of_moves, combs)
+    for elem in combs:
+        if sum(elem) <= max_length:
+            list_to_permute.append(elem)
+print(list_to_permute)
+new_list_to_permute = []
+for elem in list_to_permute:
+    list_to_append_to_permute = []
+    for nr in elem:
+        list_to_append_to_permute.append(nr)
+    new_list_to_permute.append(list_to_append_to_permute)
+print(new_list_to_permute)
+
+for elem in new_list_to_permute:
+    perms = it.permutations(elem)
+    perms = set(perms)
+    print(list(perms))
+    for perm in perms:
+        pen_final = []
+        for nr in perm:
+            pen_final.append(nr)
+        final.append(pen_final)
+
+print(final)
