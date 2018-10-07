a16937f0cd8711470a849b7ece27d837b3cbf7d4
Damian <dcamenisch98@gmail.com>
2018-10-07 19:13:53 +0200

    super math, to be continued

--- src/superMath/SupNr.java
+++ src/superMath/SupNr.java
@@ -1,13 +1,45 @@
-package superMath;
+
 import java.text.DecimalFormat;
 import java.util.*;
 
 
 public class SupNr {
-	private static int precision = 50; //must be at least 22
-	private static int nrsBeforePoint = 20;
+	private static int precision = 100; //must be at least 22
+	private static int nrsBeforePoint = 50; // (always index of first digit after decimal point)
 	int[] number = new int[precision]; //the array storing the decimal nr,
-	int posOrNeg;
+	int posOrNeg = 1;
+
+	/* constructors:
+			SupNr(double),
+			SupNr(SupNr)-> create a copy
+			SupNr(double, double), (double, double, double) -> composes a number out of multiple 10 digit integers
+				(they get appended after each other starting after the decimal point)
+		utility:
+			toString()
+			calcPi()-> return an approxmation of PI
+		Advanced Math:
+
+		Methods for addition:
+			this.add(SupNr), (double)
+			this.add(SupNr, SupNr), (double, double) -> adds two numbers in one step
+		Methods for subtraction:
+			this.subt(SupNr), (double)
+			this.isBiggerThan(SupNr), (double), -> returns true <=> this > parameter
+		Methods for multiplication:
+			this.power10(int)-> multiplies this with 10^parameter (shifts digits)
+			this.multOneDig(int)-> multiplies this by a one digit integer
+			this.mult(SupNr), (double)
+		Methods for division:
+			reciprocal(SupNr) -> returns the reciprocal of positive numbers
+			this.div(SupNr), (double)
+
+
+	*/
+
+//constructors ---------------------------------------------------------------------------------
+	public SupNr() {
+		this.number = new int[precision];
+	}
 
 	public SupNr(SupNr origin) {
 		this.posOrNeg = origin.posOrNeg;
@@ -17,7 +49,6 @@ public class SupNr {
 	}
 
 	public SupNr(double inputnr) {
-		posOrNeg = 1;
 		if (inputnr < 0) {
 			inputnr *= -1;
 			posOrNeg = -1;
@@ -31,8 +62,6 @@ public class SupNr {
 			if (strVersion.charAt(i) == '.') {
 				decimalPtIndex = i;
 				//System.out.println(decimalPtIndex);
-
-
 				break;
 			}
 		}
@@ -47,10 +76,31 @@ public class SupNr {
 		}
 	}
 
+	public SupNr(double one, double two) {
+		SupNr first = new SupNr(one);
+		SupNr second = new SupNr(two);
+		first.power10(-10);
+		second.power10(-20);
+		first.add(second);
+		this.number = first.number;
+	}
 
+	public SupNr(double one, double two, double three) {
+		SupNr first = new SupNr(one);
+		SupNr second = new SupNr(two);
+		SupNr third = new SupNr(three);
+		first.power10(-10);
+		second.power10(-20);
+		third.power10(-30);
+		first.add(second, third);
+		this.number = first.number;
+	}
+
+// utility -------------------------------------------------------------------------------------------------------------
 	public String toString() {
 		String result = "SupNr: ";
-		if (posOrNeg == 1) {
+		// System.out.println(this.posOrNeg);
+		if (this.posOrNeg == 1) {
 			result += "+ ";
 		} else {
 			result += "- ";
@@ -65,9 +115,35 @@ public class SupNr {
 		return result;
 	}
 
-// method for adding and subtracting
+	public static SupNr calcPi() {
+		SupNr PI = new SupNr();
+		SupNr four;
+		SupNr next;
+		int plusminus = 1;
+		int nexto;
+		for (int i = 1; i < 100000; i += 2) {
+			four = new SupNr(4);
+			nexto = i * plusminus;
+			next = new SupNr(nexto);
+			plusminus *= -1;
+			four.div(next);
+			PI.add(four);
+		}
+		return PI;
+	}
+
+// Advanced Maths -------------------------------------------------------------------------------
+	public void power(int power) {
+		SupNr multi = new SupNr(this);
+		for (int i = 1; i < power; i++) {
+			this.mult(multi);
+		}
+	}
+
+// methods for addition --------------------------------------------------------------------------------------
 	public void add(SupNr adder) { // increases abs value or call subtract method
 		if (this.posOrNeg == adder.posOrNeg) { // the abs value of two summands is increased if both are pos or both neg
+			// System.out.println("he says both positive");
 			int carry = 0;
 			int newDigit;
 			for (int i = precision - 1; i >= 0; i--) { // adds the values appropriately
@@ -101,7 +177,18 @@ public class SupNr {
 		this.add(adder);
 	}
 
-//everything to do the subtract stuff
+	public void add(SupNr one, SupNr two) {
+		this.add(one);
+		this.add(two);
+	}
+
+	public void add(double one, double two) {
+		SupNr first = new SupNr(one);
+		SupNr second = new SupNr(two);
+		this.add(first, second);
+	}
+
+//everything to do the subtract stuff ---------------------------------------------------------------------------------------
 	private boolean biggerComparer(SupNr bigger, SupNr that) { // algorithem that returns true if
 		// the first non-different digit of bigger is bigger than the one in thanThat:
 		int i = 0;
@@ -130,9 +217,24 @@ public class SupNr {
 		}
 		if (this.posOrNeg == 1) { //if we are dealing with positive numbers
 			return biggerComparer(this, compare);
-		} else {
+		} else { // else...
 			return biggerComparer(compare, this);
 		}
+	}
+
+	public boolean isBiggerThan(double squee) {
+		SupNr compare = new SupNr(squee);
+		return this.isBiggerThan(compare);
+	}
+
+	private boolean isBiggerThanOrEqual(double squee) {
+		SupNr compare = new SupNr(squee);
+		return this.isBiggerThan(compare) || Arrays.equals(this.number, compare.number);
+	}
+
+	public void subt(double subby) {
+		SupNr subber = new SupNr(subby);
+		this.subt(subber);
 	}
 
 	private SupNr subt(SupNr subbi, SupNr subber) { //takes two positive numbers only (subbi > subber) hence private...
@@ -187,7 +289,152 @@ public class SupNr {
 		}
 	}
 
+// method for multiplication----------------------------------------------------------------------------------------
+	public void div10() {
+		for (int i = precision - 1; i > 0; i--) {
+			this.number[i] = this.number[i - 1];
+		}
+		this.number[0] = 0;
+	}
 
+	public void mult10() {
+		for (int i = 0; i <= precision - 2; i++) {
+			this.number[i] = this.number[i + 1];
+		}
+		this.number[precision - 1] = 0;
 
+	}
+
+	public void power10(int power) {
+		if (power >= 0) {
+			for (int i = 0; i < power; i++) {
+				this.mult10();
+			}
+		} else {
+			for (int i = 0; i > power; i--) {
+				this.div10();
+			}
+		}
+	}
+
+	private void crudeMult(int multor) { // multiplies by one-digit positive integer
+		int carry = 0;
+		for (int i = precision - 1; i >= 0; i--) {
+			this.number[i] = this.number[i] * multor + carry;
+			carry = this.number[i] / 10;
+			this.number[i] = this.number[i] % 10;
+		}
+	}
+
+	public void multOneDig(int multor) { // multiplies by a one-digit integer
+		if (multor < 0) {
+			this.posOrNeg *= -1;
+			multor *= -1;
+		}
+		this.crudeMult(multor);
+	}
+
+	public void mult(SupNr multor) {
+		SupNr safeCopy = new SupNr(this);
+		safeCopy.posOrNeg = 1;
+		this.posOrNeg *= multor.posOrNeg;
+		int safePosOrNeg = this.posOrNeg;
+		this.posOrNeg = 1;
+		this.number = new int[precision];
+		for (int i = 0; i < precision; i++) {
+			SupNr nextToAdd = new SupNr(safeCopy);
+			int nextMultor = multor.number[i];
+			if (nextMultor != 0) { // maybe check later wether this decreases or increases performance
+				nextToAdd.power10(nrsBeforePoint - i - 1);
+				nextToAdd.crudeMult(nextMultor);
+				this.add(nextToAdd);
+			}
+		}
+		this.posOrNeg = safePosOrNeg;
+	}
+
+	public void mult(double multor) {
+		SupNr multy = new SupNr(multor);
+		this.mult(multy);
+	}
+
+//methods for division --------------------------------------------------------------------------------------
+	public static SupNr reciprocal(SupNr dude) { // dude is positive when coming from the divide method,
+		//must allllllllways be positive
+		SupNr orig = new SupNr(dude);
+		// System.out.println("orig dude " + orig);
+		SupNr result = new SupNr();
+		SupNr one = new SupNr(1);
+		int startingIndex = nrsBeforePoint - 1;
+		if (orig.isBiggerThan(1)) {
+			do {
+				orig.power10(-1);
+				startingIndex += 1;
+			} while (orig.isBiggerThan(1));
+		} else { // it is smaller than one (e.g. 0.000002)
+			while (!orig.isBiggerThan(1)) {
+				// System.out.println("smaller than 1");
+				orig.power10(1);
+				startingIndex -= 1;
+				if (startingIndex < -1) {
+					System.out.println("DIVISION BY ZERO! :/");
+					for (int i = 0; i < precision; i++) {
+						result.number[i] = 9;
+					}
+					return result;
+				}
+			}
+			orig.power10(-1);
+			startingIndex += 1;
+			// System.out.println(" thats the number " + orig);
+		} // now every number is of the form 0.xxxx... , the index of the first non-zero number in the result is stored in startingIndex
+		boolean stopIdiot;
+		for (int i = startingIndex; i < precision; i++) {
+			stopIdiot = false;
+			int nextNewDigit = 0;
+			while (one.isBiggerThanOrEqual(0) && !stopIdiot) {
+				one.subt(orig);
+				// System.out.println("new one " + one);
+				// System.out.println(orig);
+				nextNewDigit += 1;
+				if (nextNewDigit > 11) {
+					nextNewDigit = 1;
+					stopIdiot = true;
+				}
+			}
+
+			one.add(orig); //we remedy what we overshot
+			if (nextNewDigit > 10) { // We needed 11 subts to overshoot (10 was exactly = 0)
+				// System.out.println("bigger than 10");
+				result.number[i-1] += 1;
+				result.number[i] = 0;
+				break;
+			} else {
+				nextNewDigit -= 1;
+				result.number[i] = nextNewDigit;
+			}
+			// System.out.println("result " + result);
+			orig.power10(-1);
+			// System.out.println("the next orig --- " + orig);
+		}
+		return result;
+	}
+
+	public void div(SupNr divisor) {
+		int safeThis = this.posOrNeg *= divisor.posOrNeg;
+		this.posOrNeg = 1;
+		int safePos = divisor.posOrNeg;
+		divisor.posOrNeg = 1;
+		divisor = reciprocal(divisor);
+		// System.out.println(divisor);
+		this.mult(divisor);
+		divisor.posOrNeg = safePos;
+		this.posOrNeg = safeThis;
+	}
+
+	public void div(double divisor) {
+		SupNr squee = new SupNr(divisor);
+		this.div(squee);
+	}
 
 }
--- src/superMath/SuperMath.java
+++ src/superMath/SuperMath.java
@@ -1,15 +1,32 @@
-package superMath;
+
 // a program which can calculate with numbers beyond the precision of built-in doubles :O
 
 public class SuperMath {
 	public static void main(String[] args) {
-		SupNr testnr = new SupNr(452.999999);
-		testnr.add(0.0001);
-//		SupNr testnr2 = new SupNr(453.9998);
-//		System.out.println(testnr.isBiggerThan(testnr2));
-//		testnr.subt(testnr2);
-		System.out.println(testnr);
-//		System.out.println(testnr2);
+
+		// SupNr testnr = new SupNr(1234567890.0, 2345678901.0, 234781234.0);
+		SupNr testnr = SupNr.calcPi();
+
+
+		// SupNr testnr2 = new SupNr(17);
+		// testnr.mult(testnr2);
+		// testnr = SupNr.reciprocal(testnr);
+		// testnr = SupNr.reciprocal(testnr);
+		// testnr.add(12, 1130);
+		// testnr.multOneDig(-4);
+		// SupNr testnr2 = new SupNr(3.223948739);
+		// testnr.power10(10);
+		// System.out.println(testnr + "\ndivided by \n" + testnr2);
+		// // testnr.subt(testnr2);
+		// testnr.div(testnr2);
+		System.out.println("this is testnr " + testnr);
+		// // testnr = SupNr.reciprocal(testnr);
+		// System.out.println("is:\n" + testnr);
+		// // // testnr.mult(5);
+		// // System.out.println(testnr2);
+		// // System.out.println(5 * 5.63458);
+		//
+
 
 
 	}
